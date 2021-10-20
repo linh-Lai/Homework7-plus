d38686b5609941ca2a49129dd6d62508d148dec7
linh-Lai <92214613+linh-Lai@users.noreply.github.com>
2021-10-20 22:41:33 +0200

    updated forecast

--- script.js
+++ script.js
@@ -21,6 +21,32 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Son", "Mon"];
+  days.forEach(function (day){
+
+    forecastHTML = forecastHTML + `
+      <div class="col-2">
+        <div class="weather-forecast-date"> ${day} </div>
+        <img src="" alt="" width="42" />
+        <div class="weather-forecast-temp">
+          <span class="weather-forecast-temp-max"> 18 </span>
+          <span class="weather-forecast-temp-min"> 12 </span>
+       </div>
+      </div>`;
+});
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+}
+function getForecast(coordinates) {
+  let apiKey = "d6ff37a4c170f249f41b8f3e7f2c9d0a";
+  let apiUrl =
+`https://openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`
+axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let location = document.querySelector("#city");
   let temperatureElement = document.querySelector("#temperature");
@@ -43,6 +69,8 @@ function displayTemperature(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -86,3 +114,4 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 search("Berlin");
+displayForecast();
